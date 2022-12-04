c2d057b56abfeca3e418c664e34c86b18de1a79d
Guerrero16 <rjt1608@gmail.com>
2022-12-04 11:38:54 -0500

    ejercicio4_1

---
+++ src/main/java/com/raul/ejercicios_tema4/Ejercicios_tema_4_1.java
@@ -0,0 +1,25 @@
+/*
+Usando un if, crear una condición que compare si la variable numeroIf es positivo, negativo, o 0.
+Pista: Los números inferiores a 0 son negativos y los superiores, positivos.
+ */
+
+package com.raul.ejercicios_tema4;
+
+/**
+ *
+ * @author R-pc
+ */
+public class Ejercicios_tema_4_1 {
+
+    public static void main(String[] args) {
+        int numeroIf = -4;
+        
+        if (numeroIf == 0) {
+			System.out.println("El número es neutro");
+		} else if (numeroIf < 0) {
+			System.out.println("El número es negativo");
+		} else {
+			System.out.println("El número es positivo");
+		}
+    }
+}
